735b45c90eb3617e0ddd3850627a7e9fac638460
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2023-03-21 09:25:37 +0300

    Add files via upload

---
+++ Johnny Bravo Date o Rama! [PS2toPS4] [PAL] FiX/SLES-54671_config.lua
@@ -0,0 +1,16 @@
+-- Johnny Bravo Date o Rama!
+-- emu used=ADK
+
+apiRequest(1.0)   
+
+local gpr = require("ee-gpr-alias")
+
+local eeObj = getEEObject()
+local emuObj = getEmuObject()
+
+local patcher = function()
+
+emuObj.ThrottleMax()
+end
+
+emuObj.AddVsyncHook(patcher)
