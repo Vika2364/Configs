3eafb375de9201720365056f1fd0a80bd2a99dba
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2023-04-17 17:28:16 +0300

    Add files via upload

---
+++ NiGHTS into Dreams [PS2toPS4] [NTSJ] FiX/SLPM-66926_config.lua
@@ -0,0 +1,16 @@
+-- NiGHTS Into Dreams (JPN)
+-- emu used=ADK
+
+apiRequest(1.0)   
+
+local gpr = require("ee-gpr-alias")
+
+local eeObj = getEEObject()
+local emuObj = getEmuObject()
+
+local patcher = function()
+
+emuObj.ThrottleMax()
+end
+
+emuObj.AddVsyncHook(patcher)
