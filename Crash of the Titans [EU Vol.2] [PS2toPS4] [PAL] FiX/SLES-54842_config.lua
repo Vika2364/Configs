308a922e855bf7620a9dab0f808529655565fbe6
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2023-07-13 22:32:14 +0300

    Add files via upload

---
+++ Crash of the Titans [EU Vol.2] [PS2toPS4] [PAL] FiX/SLES-54842_config.lua
@@ -0,0 +1,16 @@
+-- Crash of the Titans (PAL)
+-- emu used=JakX v2 
+
+apiRequest(1.0)   
+
+local gpr = require("ee-gpr-alias")
+
+local eeObj = getEEObject()
+local emuObj = getEmuObject()
+
+local patcher = function()
+
+emuObj.ThrottleMax()
+end
+
+emuObj.AddVsyncHook(patcher)
