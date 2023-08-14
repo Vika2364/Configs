68a2d243507faff8732d9eb9713b79b90db92200
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2023-08-14 13:49:46 +0300

    Add files via upload

---
+++ Legacy of Kain Soul Reaver [PSXtoPS4] [NTSC] FiX/SLUS00708.lua
@@ -0,0 +1,13 @@
+-- Legacy Of Kain Soul Reaver (NTSC)
+-- ported to PS4 lua
+-- emu used=syphonfilter 3
+local patcher = function()
+--Widescreen
+R3K_WriteMem16(0x8003A374,0x0C00)
+R3K_WriteMem16(0x8003A384,0x0034)
+R3K_WriteMem16(0x8003A386,0xAFA0)
+R3K_WriteMem16(0x8003A388,0x1000)
+R3K_WriteMem16(0x8003A38A,0x2403)
+end
+EM_AddVsyncHook(patcher)
+
