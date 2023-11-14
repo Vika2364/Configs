b455f141723d93e362013e4e66a09dcda49c4a5e
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2023-11-14 03:06:21 +0300

    Add files via upload

---
+++ Ibara [PS2toPS4] [NTSJ] FiX/SLPM-66301_config.lua
@@ -0,0 +1,17 @@
+-- Ibara (NTSC-J)
+-- emu used=ADK
+
+apiRequest(1.0)   
+
+local gpr = require("ee-gpr-alias")
+
+local eeObj = getEEObject()
+local emuObj = getEmuObject()
+
+local patcher = function()
+
+
+emuObj.ThrottleMax()
+end
+
+emuObj.AddVsyncHook(patcher)
