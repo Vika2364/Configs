cf16699546038dd194d6e11dc9445a3a4c46e144
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2023-12-25 02:21:32 +0300

    Add files via upload

--- The Grinch [PSXtoPS4] [NTSC] FiX/SLUS01197.lua
+++ The Grinch [PSXtoPS4] [NTSC] FiX/SLUS01197.lua
@@ -1,6 +1,6 @@
--- The Grinch
+-- The Grinch (NTSC)
 -- ported to PS4 lua
--- emu used=syphonfilter v2
+-- emu used=syphonfilter 3
 local patcher = function()
 --Widescreen
 R3K_WriteMem16(0x8009A704,0x0C00)
---
+++ The Grinch [PSXtoPS4] [NTSC] FiX/app_boot.lua
@@ -0,0 +1,7 @@
+require "disc-selection"
+
+local pal_disc_map = { 
+	en = { "TheGrinch_SLUS01197_SCEA_EN.bin" }
+}
+
+RegisterDiscs(pal_disc_map, ntsc_disc_map)
