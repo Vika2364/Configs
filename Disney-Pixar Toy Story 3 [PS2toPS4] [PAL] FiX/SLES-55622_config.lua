3475d3f3775a25d72fd49ca7e2738db43fd65e3c
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2024-01-12 14:46:56 +0300

    Add files via upload

---
+++ Disney-Pixar Toy Story 3 [PS2toPS4] [PAL] FiX/SLES-55622_config.lua
@@ -0,0 +1,27 @@
+-- Disney-Pixar's Toy Story 3 (SLES55622)
+-- Widescreen by Arapapa
+-- Jak v2
+
+apiRequest(0.1)
+
+local gpr = require("ee-gpr-alias")
+local eeObj = getEEObject()
+local emuObj = getEmuObject()
+
+local patcher = function()
+
+--Widescreen hack 16:9
+
+--Zoom fix (Internal Widescreen)
+--aa3f033c 0040023c 713d6334
+eeObj.WriteMem32(0x004f6ee8,0x3c033f80)
+eeObj.WriteMem32(0x004f6ef0,0x34630000)
+
+end
+emuObj.AddVsyncHook(patcher)
+
+emuObj.SetGsTitleFix( "forceSimpleFetch", "reserved", { texMode=1 } ) 
+emuObj.SetGsTitleFix( "forceSimpleFetch", "reserved", { texMode=2 } ) 
+
+emuObj.SetGsTitleFix( "forceSimpleFetch",  "reserved", {psm=0} )
+emuObj.SetGsTitleFix( "fetchFromCurrBuff", "reserved", {psm=0} )
