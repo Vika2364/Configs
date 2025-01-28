42987dd89db2a62429dace6427debb22f8ba863c
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2025-01-29 01:16:49 +0300

    Add files via upload

---
+++ Dead to Rights [PS2toPS4] [PAL] FiX/SLES-51581_config.lua
@@ -0,0 +1,26 @@
+-- Dead to Rights [SLES-51581] (E)
+-- Widescreen hack by Arapapa
+-- Jak v2
+
+local gpr = require("ee-gpr-alias")
+
+apiRequest(0.1)
+
+local eeObj = getEEObject()
+local emuObj = getEmuObject()
+
+local patcher = function()
+
+--Widescreen hack 16:9
+
+--Force turn on Widescreen
+--00 00 00 00 01 00 00 00 AB AA AA 3F 00 00 80 3F
+eeObj.WriteMem32(0x2042FBE0,0x00000001)
+
+--Zoom fix
+--29 5C 8F 3F 00 00 C0 43 00 00 C0 43
+eeObj.WriteMem32(0x2042FBF4,0x3f800000) --3f8f5c29
+
+end
+
+emuObj.AddVsyncHook(patcher)
