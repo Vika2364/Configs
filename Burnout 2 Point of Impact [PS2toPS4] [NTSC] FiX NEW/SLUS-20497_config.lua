1ba52c576b0e901e62a074da4b80dd15f28d362c
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2025-02-03 06:00:03 +0300

    Add files via upload

---
+++ Burnout 2 Point of Impact [PS2toPS4] [NTSC] FiX NEW/SLUS-20497_config.lua
@@ -0,0 +1,13 @@
+-- Burnout 2 Point of Impact (U)
+-- emu used=rogue v1
+
+apiRequest(0.1)    -- request version 0.1 API. Calling apiRequest() is mandatory.
+
+-- SLUS_204.97 - Fix white parts
+-- Major thanks to Maori-Jigglypuff for reversing that
+
+eeInsnReplace(0x2485A8, 0xC6800D18, 0x3C014316)
+eeInsnReplace(0x2485BC, 0xE4400000, 0xAC410000)
+eeInsnReplace(0x2625F4, 0xC7809780, 0x3C014316)
+eeInsnReplace(0x262608, 0xE4400000, 0xAC410000)
+eeInsnReplace(0x2564A8, 0x8F829764, 0x24020000)
