9f7df365ffc41aadd4e681292e8cf4114479cd44
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2025-03-07 07:01:57 +0300

    Add files via upload

---
+++ BloodRayne [PS2toPS4] [PAL] FiX/SLES-51474_config.lua
@@ -0,0 +1,17 @@
+-- Bloodrayne SLES_514.74
+-- emu used=ADK
+
+apiRequest(1.0)   
+
+local gpr = require("ee-gpr-alias")
+
+local eeObj = getEEObject()
+local emuObj = getEmuObject()
+
+local patcher = function()
+
+
+emuObj.ThrottleMax()
+end
+
+emuObj.AddVsyncHook(patcher)
