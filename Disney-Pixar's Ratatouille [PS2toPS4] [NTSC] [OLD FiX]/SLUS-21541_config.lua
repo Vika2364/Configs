4ed4ecbc4a6e3cab63294aa3d162b624ae04082f
Vika2364 <116828182+Vika2364@users.noreply.github.com>
2025-06-09 10:54:43 +0300

    Add files via upload

---
+++ Disney-Pixar's Ratatouille [PS2toPS4] [NTSC] [OLD FiX]/SLUS-21541_config.lua
@@ -0,0 +1,26 @@
+-- Ratatouille NTSC
+-- ported to PS4 LUA
+-- emu used=aofa
+
+apiRequest(2.0)
+
+local gpr       = require("ee-gpr-alias")
+
+local emuObj      = getEmuObject()
+local eeObj       = getEEObject()
+local gsObj       = getGsObject()
+
+emuObj.SetDisplayAspectWide()
+  
+local Main = function()
+
+   eeObj.SchedulerDelayEvent("vif0.dma", 0x6500) -- used as speed up
+   eeObj.SchedulerDelayEvent("gif.dma", 0x1000) -- 
+   emuObj.ThrottleMax()
+
+end 
+
+emuObj.AddVsyncHook(Main)
+
+emuObj.SetGsTitleFix( "forceSimpleFetch", "reserved", { texMode=1  } ) -- graphics fix A
+emuObj.SetGsTitleFix( "forceSimpleFetch", "reserved", { texMode=2  } ) -- graphics fix B
